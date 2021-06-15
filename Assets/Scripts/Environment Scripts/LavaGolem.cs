d1fb750f14ae1b4436319f0f902ba3bc92410299
Kontrazis Petros <55915800+petrosKon@users.noreply.github.com>
2021-06-15 18:55:43 +0200

    Third scene completed

---
+++ Assets/Scripts/Environment Scripts/LavaGolem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaGolem : MonoBehaviour
+{
+    private Vector3 direction;
+
+    public GameObject player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = (player.transform.position - transform.position).normalized;
+
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+}
