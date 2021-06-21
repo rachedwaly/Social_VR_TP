9cbababe3035b135fe6db552da0f53a322a02718
Rached Waly <rachedwaly1998@gmail.com>
2021-06-21 11:21:33 +0200

    two images

--- Assets/Scripts/WebCamDisplay.cs
+++ Assets/Scripts/WebCamDisplay.cs
@@ -8,14 +8,15 @@
 
     public class WebCamDisplay : MonoBehaviour, IWebCamDisplay
     {
-        public Camera selfieCam, selfieCam2;
+        //public Camera selfieCam, selfieCam2;
 
         private WebCamTexture webCamTexture;
         private bool takeSelfie = false;
-        private Camera virtualCamera, virtualCamera2;
+        private Camera virtualCamera, virtualCamera2, selfieCam, selfieCam2;
         private Transform target;
         private Vector3 screenPos;
         private Rect rect1;
+        private Rect rect2;
 
         // Start is called before the first frame update
 
@@ -54,7 +55,7 @@
 
 
                 selfieCam.CopyFrom(virtualCamera);
-                selfieCam2.CopyFrom(virtualCamera2);
+                //selfieCam2.CopyFrom(virtualCamera2);
 
 
                 var currentRT = RenderTexture.active;
@@ -94,10 +95,7 @@
                 Texture2D texture2D = new Texture2D(webCamTexture.width, webCamTexture.height);
                 texture2D.SetPixels32(webCamTexture.GetPixels32());
 
-                Debug.Log("zall3 " + selfieCam.targetTexture.width);
-                Debug.Log("zall3 " + selfieCam.targetTexture.height);
-                Debug.Log("zall3 " + webCamTexture.width);
-                Debug.Log("zall3 " + webCamTexture.height);
+
 
 
 
@@ -107,9 +105,9 @@
                 Mat test = Unity.TextureToMat(texture2D);
                 Cv2.Circle(test, new Point(screenPos.x + 50, screenPos.y), 5, new Scalar(255,0,0));
 
-                //Cv2.Rectangle(test, new Rect((int)screenPos.x, (int)screenPos.y, -150, -150), new Scalar(255, 0, 0));
-                rect1 = new Rect((int)(test.Width * 0.20), (int)(test.Height * 0.20), (int)(test.Width * 0.6), (int)(test.Height * 0.79));
-                Cv2.Rectangle(test, new Rect((int)(test.Width * 0.20), (int)(test.Height * 0.20), (int)(test.Width * 0.6), (int)(test.Height * 0.79)), new Scalar(255, 0, 0));
+                rect1 = new Rect((int)(test.Width * 0.20), (int)(test.Height * 0.05), (int)(test.Width * 0.6), (int)(test.Height * 0.95));
+                rect2 = new Rect((int)(test.Width * 0.20), (int)(test.Height *0.3), (int)(test.Width * 0.6), (int)(test.Height * 0.68));
+                //Cv2.Rectangle(test, rect1, new Scalar(255, 0, 0));
                 texture2D = Unity.MatToTexture(test);
 
                 Mat test1 = Unity.TextureToMat(image) ;
@@ -146,41 +144,32 @@
 
             Texture2D real_selfie = LoadPNG(Application.dataPath + "/Resources/SelfieIRL.png");
 
-            Texture2D virtual_selfie = LoadPNG(Application.dataPath + "/Resources/VSelfieIRL.png");
+            Texture2D virtual_selfie = LoadPNG(Application.dataPath + "/Resources/VSelfieIRLBG.png");
 
-            Debug.Log("zall4 " + real_selfie.width);
-            Debug.Log("zall4 " + real_selfie.height);
-            Debug.Log("zall4 " + virtual_selfie.width);
-            Debug.Log("zall4 " + virtual_selfie.height);
+            Texture2D virtual_selfie_fullgame = LoadPNG(Application.dataPath + "/Resources/VSelfieIRL.png");
 
 
 
 
-            //Texture2D my_img = LoadPNG(Application.dataPath + "/Resources/zall1.png");
             Mat real_matc = Unity.TextureToMat(real_selfie);
             Mat virtual_matc = Unity.TextureToMat(virtual_selfie);
-            Debug.Log("zall1 " + real_matc.Size());
-            Debug.Log("zall2 " + virtual_matc.Size());
+            Mat fullvirtual_matc = Unity.TextureToMat(virtual_selfie_fullgame);
 
 
-            Mat result = new Mat(real_matc.Size(), MatType.CV_8UC1);
 
-            //Rect rect = new Rect(2, 2, matc.Width - 2, matc.Height - 2);
-
-
-            
-
+            /*
             int x = System.Math.Min((int)(screenPos.x - (real_matc.Width / 5.0)), 0);
             int y = System.Math.Min((int)(screenPos.y - real_matc.Height / 5.0), 0);
             int w = (int)(real_matc.Width * (2.0 / 5.0));
             int h = (int)(real_matc.Height *(2.0 / 5.0));
+            */
 
-            //Rect rect = new Rect((int)(real_matc.Width * (1.0 / 4.0)), (int)(real_matc.Height * (1.0 / 4.0)),(int)(real_matc.Width * (1.0 / 2.0)) , (int)(real_matc.Height * (1.0 / 2.0)));
-            Rect rect = new Rect((int)screenPos.x, (int)screenPos.y, -150, -150);
-            //Rect rect = new Rect((int)rectangle.p1.x, (int)rectangle.p1.y, (int)rectangle.GetWidth(), (int)rectangle.GetHeight());
+
+            Mat result_mask = new Mat(real_matc.Size(), MatType.CV_8UC1);
             
-            Cv2.GrabCut(virtual_matc, result, rect1, new Mat(), new Mat(), 3, GrabCutModes.InitWithRect);
-            Mat mask1 = ((result & 1)) * 255;
+            Cv2.GrabCut(virtual_matc, result_mask, rect1, new Mat(), new Mat(), 3, GrabCutModes.InitWithRect);
+            
+            Mat mask1 = ((result_mask & 1)) * 255;
             Mat mask2 = new Mat(mask1.Size(), MatType.CV_8UC1); ;
             mask2 = 255 - mask1;
 
@@ -191,8 +180,23 @@
             virtual_matc.CopyTo(cropped, mask1);
 
 
-            Debug.Log("zall" + x);
-            Debug.Log("zall" + y);
+            Mat result_mask_real = new Mat(real_matc.Size(), MatType.CV_8UC1);
+
+            
+
+            Cv2.GrabCut(real_matc, result_mask_real, rect1, new Mat(), new Mat(), 3, GrabCutModes.InitWithRect);
+
+            Mat mask1_real = ((result_mask_real & 1)) * 255;
+            Mat mask2_real = new Mat(mask1.Size(), MatType.CV_8UC1); ;
+            mask2_real = 255 - mask1_real;
+
+            //applying the mask
+            Mat cropped_real = new Mat();
+
+            fullvirtual_matc.CopyTo(cropped_real, mask2_real);
+            real_matc.CopyTo(cropped_real, mask1_real);
+
+
 
             //Mat grayMat = new Mat();
             // Mat result_inpaint = new Mat();
@@ -202,13 +206,18 @@
             //  Cv2.Circle(result_inpaint, new Point(screenPos.x, screenPos.y), 10, 1);
 
             //Cv2.Rectangle(cropped, rect, )
-
+            byte[] byteArray6 = Unity.MatToTexture(cropped_real).EncodeToJPG();
+            //byte[] byteArray2 = Unity.MatToTexture(cropped).EncodeToJPG();
+            System.IO.File.WriteAllBytes(Application.dataPath + "/Resources/result_real.png", byteArray6);
+            AssetDatabase.Refresh();
 
 
             byte[] byteArray5 = Unity.MatToTexture(cropped).EncodeToJPG();
             //byte[] byteArray2 = Unity.MatToTexture(cropped).EncodeToJPG();
             System.IO.File.WriteAllBytes(Application.dataPath + "/Resources/result.png", byteArray5);
             AssetDatabase.Refresh();
+
+      
 
         }
 
